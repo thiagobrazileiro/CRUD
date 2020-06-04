2b073ca013e3e7bec307a8ea54bc76cbf9303a87
Thiago Brazileiro <thiago.brazileiro@hotmail.com>
2020-06-04 20:02:20 -0300

    :art: Melhora a mensagem de cadastro
    A mensagem de cadastro concluido com sucesso está melhorada.

--- src/Includes/FormularioCadastro.php
+++ src/Includes/FormularioCadastro.php
@@ -1,4 +1,9 @@
 <?php
+/*
+Se vier um ID como referência, ele já sabe que é para editar
+e com isso ele ja deixa os campos pré-preenchidos. Também da
+valor a variavel "Acao", para poder executar os scripts corretamente.
+*/
 include("Class/ClassCrud.php");
 
     if(isset($_GET['id'])){
@@ -21,58 +26,58 @@ include("Class/ClassCrud.php");
         $Instituicao = "";
         $Curso = "";
     }
-
 ?>
-   		<!-- Modal -->
-		<div class="modal fade resultado" id="modalSucesso" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-		  <div class="modal-dialog" role="document">
-			<div class="modal-content">
-			  <div class="modal-header">
+
+<!-- Modal -->
+<div class="modal fade resultado" id="modalSucesso" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
 				<h5 class="modal-title" id="exampleModalLabel">Sucesso!</h5>
 				<button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-				  <span aria-hidden="true">&times;</span>
+					<span aria-hidden="true">&times;</span>
 				</button>
-			  </div>
-			  <div class="modal-body">
-				Cadastro Realizado com Sucesso!
-			  </div>
-			  <div class="modal-footer">
-				<button type="button" onClick="window.location.reload()" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
-			
-			  </div>
 			</div>
-		  </div>
+			<div class="modal-body">
+				<?php 
+					if($Acao == "Cadastrar"){
+						echo("Cadastrado com sucesso!"); 
+					}else{
+						echo("Editado com sucesso!");
+					}
+				?>
+			</div>
+			<div class="modal-footer">
+				<button type="button" onClick="window.location.reload()" class="btn btn-primary" data-dismiss="modal">Fechar</button>
+			</div>
 		</div>
+	</div>
+</div>
 		
+<!-- Formulario começa aqui -->    
     <div class="Formulario">
         <h1 class="Center">Cadastro</h1>
-		
-
-		
         <form name="FormCadastro" id="FormCadastro" method="post" action="Controllers/ControllerCadastro.php">
             <input type="hidden" id="Acao" name="Acao" value="<?php echo $Acao; ?>">
             <input type="hidden" id="Id" name="Id" value="<?php echo $Id; ?>">
 
             <div class="FormularioInput">
-                Nome: <br>
-                <input type="text" id="Nome" name="Nome" value="<?php echo $Nome; ?>">
+            	Nome: <br>
+            	<input type="text" id="Nome" name="Nome" value="<?php echo $Nome; ?>">
             </div>
 
             <div class="FormularioInput">
-                Instituicao: <br>
-                <input type="text" id="Instituicao" name="Instituicao" value="<?php echo $Instituicao; ?>">
+            	Instituicao: <br>
+            	<input type="text" id="Instituicao" name="Instituicao" value="<?php echo $Instituicao; ?>">
             </div>
 
             <div class="FormularioInput">
-                Curso: <br>
-                <input type="text" id="Curso" name="Curso" value="<?php echo $Curso; ?>">
+            	Curso: <br>
+            	<input type="text" id="Curso" name="Curso" value="<?php echo $Curso; ?>">
             </div>
 
             <div class="FormularioInput FormularioInput100 Center">
-
 				<input type="submit" value="<?php echo $Acao; ?>" class="btn btn-primary" data-toggle="modal" data-target="#modalSucesso">
-				
             </div>
         </form>
-
     </div>
